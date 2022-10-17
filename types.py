29c943e89491eef525a9597c5a31272335ce0d40
jnooriRS <jonas.noori@riversafe.co.uk>
2022-10-17 11:56:49 +0100

    types and interpolation

---
+++ types.py
@@ -0,0 +1,12 @@
+# Exercise - Create variables for the following data types: string, int, float, boolean, array 
+# Use a print function to print the content of those variables 
+# Stretch goal - Experiment with formatting your print statement to make it interesting 'print("price: " + price)'
+
+
+string = "printed string"
+print(string)
+
+int = 5
+print(int)
+
+print(f"price = ${int} for !{string}! to be your logo on a shirt")
