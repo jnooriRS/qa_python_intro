91b57b52698ea48441790c3705aaad82e4f2e07a
jnooriRS <jonas.noori@riversafe.co.uk>
2022-10-18 09:47:53 +0100

    factorial created

---
+++ Factorial.py
@@ -0,0 +1,6 @@
+user_input = int(input("what number factorial would you like to find...."))
+number = 1
+while user_input >= 1:
+        number = number * user_input
+        user_input = user_input - 1
+print(number)
