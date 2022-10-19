e22c3f63d068132027e9c4fae9f90c144e5a854c
jnooriRS <jonas.noori@riversafe.co.uk>
2022-10-19 15:25:07 +0100

    average but not iteration

---
+++ iteration_average.py
@@ -0,0 +1,11 @@
+from re import I
+
+
+math_result = int(input("What is your Math reult "))
+english_result = int(input("What is your English reult "))
+ict_result = int(input("What is your ICT reult "))
+
+set_of_results = (math_result, english_result, ict_result)
+average_mark = sum(set_of_results) / len(set_of_results)
+print(f"The average grade for all three exams you sat is {average_mark}")
+
