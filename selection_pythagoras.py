744ebf607dc04ab4514d7e1b3f2a56d186a68654
jnooriRS <jonas.noori@riversafe.co.uk>
2022-10-19 10:59:19 +0100

    display set up

---
+++ selection_pythagoras.py
@@ -0,0 +1,7 @@
+calculator_display = int(input('''
+Pythagoras Calculator:
+1. Find the length of A given B and C
+2. Find the length of B given A and C
+3. Find the length of C given A and B
+>>>>>>>
+'''))
