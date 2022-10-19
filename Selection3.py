3e7336182af5744b72fd8597df47b694ed09c979
jnooriRS <jonas.noori@riversafe.co.uk>
2022-10-19 10:22:14 +0100

    exam grade converter1

---
+++ Selection3.py
@@ -0,0 +1,13 @@
+
+mark_input =int(input("Student mark >>>>"))
+if mark_input < 1 or mark_input > 100:
+    print("Error: marks must be between 1 and 100")
+elif mark_input < 50:
+    print("Fail")
+elif mark_input == 50 or mark_input < 61:
+    print("Pass")
+elif mark_input == 61 or mark_input < 71:
+    print("Merit")
+else:
+    print("Distinction")
+
