6589b082f5c82dc12cd27637e2c500fbbe6e5f69
jnooriRS <jonas.noori@riversafe.co.uk>
2022-10-21 12:06:41 +0100

    gettar chaging attribute

--- class_animal_loop.py
+++ class_animal_loop.py
@@ -1,13 +1,13 @@
 class Animal:
-    def __init__(self, name, age, type, location, chosen_attr):
+    def __init__(self, name, age, type, location, diet):
         self.age = age,
         self.name = name,
         self.type = type,
         self.location = location
-        self.chosen_attr = chosen_attr
+        self.diet = diet
     
     def __str__(self):
-        return f"{self.age} + {self.name}  {self.type} + {self.location} + {self.chosen_attr} "
+        return f"{self.age} + {self.name}  {self.type} + {self.location} + {self.diet} "
 
 animal1 = Animal(31, "Jonas", "mamal", "Denmark", "Carinovire")
 animal2 = Animal(33, "Yonas", "mamal", "England", "Vegan")
@@ -16,6 +16,10 @@ print(animal2)
 
 print(getattr(animal1, 'type'))
 print(animal1.type)
+
+
+chosen_attr = "diet"
+print(getattr(animal1, chosen_attr))
 
 # hasattr - checks if this object contains this attribute (returns True or False)
 print(hasattr(animal2, 'location')) # Used to check if an attribute is there for avoiding exceptions 
@@ -26,6 +30,6 @@ print(setattr(animal2, 'location', 'Denmark')) # returns None, because setattr d
 print(hasattr(animal2, 'location'))
 
 # delattr - Takes in object, attribute name and removes this attribute (not just value)
-print(f"Location is: {animal1.chosen_attr}")
+print(f"Location is: {animal1.diet}")
 delattr(animal1, 'location')
 print(animal1.location)
